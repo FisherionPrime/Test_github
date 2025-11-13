e5b4b47a528820a915598dd0814b189299c2fe62
Ewan <ewangarn83@gmail.com>
2025-11-13 14:38:04 +0100

    Add files via upload

---
+++ dés.js
@@ -0,0 +1,18 @@
+function dés(){
+    let a_trouver = 66;
+    do{
+         var valeur = prompt("Saisissez une valeur");
+         if(valeur > a_trouver){
+            alert("trop grand")
+         }
+         else if( valeur < a_trouver){
+            alert("Trop petit ")
+         }
+         else{
+             alert("Bien joué");
+         }
+    }
+    while(a_trouver != valeur);
+}
+
+console.log(dés())
